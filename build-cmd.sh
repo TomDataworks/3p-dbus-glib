41eada5294f410ac61b010aac2d7ec177ce2030d
andrew_linden <devnull@localhost>
2011-02-18 12:21:42 -0800

    adding atuobuild.xml and build-cmd.sh

---
+++ build-cmd.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+set -x
+# make errors fatal
+set -e
+
+PROJECT="dbus-glib"
+VERSION="0.92"
+SOURCE_DIR="$PROJECT-$VERSION"
+
+if [ -z "$AUTOBUILD" ] ; then 
+    fail
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+fi
+
+# load autbuild provided shell functions and variables
+set +x
+eval "$("$AUTOBUILD" source_environment)"
+set -x
+
+stage="$(pwd)/stage"
+pushd "$SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+        "windows")
+			echo "dbus-glib headers only for linux"
+        ;;
+        "darwin")
+			echo "dbus-glib headers only for linux"
+        ;;
+        "linux")
+			# copy just the headers to the right place
+			mkdir -p $stage/dbus/
+			cp dbus/*\.h $stage/dbus/
+        ;;
+    esac
+    mkdir -p "$stage/LICENSES"
+    tail -n 31 COPYING > "$stage/LICENSES/zlib.txt"
+popd
+
+pass
+
